b3678174c6ea65e88ef8cb0c1bc3e1f60719be98
ma-foyez <mafoyez.web@gmail.com>
2022-05-13 00:48:59 +0600

    responsive issue fixed

--- public/js/main.js
+++ public/js/main.js
@@ -16,6 +16,23 @@ $(document).ready(function () {
         $('.open-settings').toggle('slide');
     });
 
+
+    /*************** Responsive Sidebar *********** */
+    $('.mobile-sidebar').removeClass('hidden');
+    $('.mobile-sidebar').hide();
+
+    $('.sidebar-toggle').on('click', function () {
+        $('.mobile-sidebar').toggle('slide');
+    });
+
+    $('.sidebar-toggle').on('click', function (event) {
+        event.stopPropagation();
+    });
+
+    $('.mobile-sidebar').on('click', function (event) {
+        event.stopPropagation();
+    }); 
+
     /*************** Store Theme in local storage *********** */
     if (localStorage.theme === 'dark') {
         document.documentElement.classList.add('dark')
